5ef43f66a2b6538fe80e058f9efba7cdc327578e
Artur Daschevici <artur.daschevici@nearform.com>
2018-11-15 17:02:17 +0000

    Add extra documentation, clean up code, add QR functionality

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ The targeted features for this are:
  - Camera access + taking pictures
  - Geolocation
  - Native Share capabilities
+ - QR Code Scanning
 
 
 ## Prerequisites:
@@ -35,6 +36,9 @@ No build step required as it is plain JS.
 
 ## Implementation:
 
+## Stage 1
+### I. Picture capture and sharing:
+
 1) For the geo location the [Geolocation API](https://developer.mozilla.org/en-US/docs/Web/API/Geolocation) was used.
 This API has [fairly wide spread adoption](https://caniuse.com/#search=geolocation) across browsers
 
@@ -43,6 +47,8 @@ This API has [fairly wide spread adoption](https://caniuse.com/#search=geolocati
 3) For sharing using the native provided menu the [Web Share API](https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share) was used however for our purposes this has [limitations](https://caniuse.com/#feat=web-share).
   - Support is only available in Chrome and Safari Tech Preview which makes it unlikely that it would work on most clients.
   - Another downside of this is that it does not seem to work for [sharing image objects](https://github.com/WICG/web-share/issues/12)
+
+
 
 ## Other potential limitations:
 1) The native camera app usually goes to postprocessing after a picture has been taken. With the PWA approach this is not the case as the image is captured to canvas then put into a `blob` type object so the post processing will no longer happen so quality/image filters may differ from what the native camera app shows. This is however the case with most native apps used.
@@ -66,8 +72,32 @@ This API has [fairly wide spread adoption](https://caniuse.com/#search=geolocati
 2) WebRTC investigation showed that the protocol is a P2P protocol and it would require a 3rd party service to buffer the files, and is connection based so it is not very much in line with how a `serviceWorker` functions. It is not a viable solution for the task as it requires a connection oriented protocol for communication.
 
 3) There is a way to share content via Deep Links to each of the apps mentioned. The caveats with this are:
-  - the share menu is not the native one, it will be part of the web app
-  - the sharing options are hard coded in the site so people will not have the same menu as they would normally have in their share context natively (ie: some people may have Twitter as their goto for sharing instead of facebook)
-  - this allows for sending **links only**
-  - it has cross platform compatibility, better than the web share API so it can be a good fallback for lacking Web Share API support on iOS
-  - could work only if images are uploaded to a 3rd party and the link is provided
+    - the share menu is not the native one, it will be part of the web app
+    - the sharing options are hard coded in the site so people will not have the same menu as they would normally have in their share context natively (ie: some people may have Twitter as their goto for sharing instead of facebook)
+    - this allows for sending **links only**
+    - it has cross platform compatibility, better than the web share API so it can be a good fallback for lacking Web Share API support on iOS
+    - could work only if images are uploaded to a 3rd party and the link is provided
+
+## Stage 2
+
+### II. QR Code Scanning:
+
+1) We used a very good QR code/barcode scanning library called [ZXing](https://github.com/zxing/zxing) with a more recent port to JS, [jsQR](https://github.com/cozmo/jsQR). `jsQR` is only targeted at reading the QR codes, so much lighter than the `ZXing` lib. The way it works:
+   - select the camera you wish to scan with on the device
+   - point it at the QR code
+   - tap the scan QR code and if the QR code is detected in the image the input below will be populated with the decoded value
+   - otherwise you need to tap the retry button to get back into picture taking mode and try again.
+  
+2) Some improvements on this:
+   - the video feed continuously checks if a QR code is detected and prints out the decoding if it is
+   - the video feed has a visual cue when a QR code is detected so that you can take the snapshot
+
+3) The implementation is very similar to the picture taking except in the QR code case the goal is retrieving the code in the QR image.
+
+## Conclusion:
+1) This is a very workable solution and for prototype purposes it worked as expected.
+2) The range at which the QR code is detected is not very big (10-20cm for a 200x200px QR code)
+
+
+## Stage 3
+## TBD
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,26 +1,17 @@
 let snapshotButton = document.querySelector('#capture-button')
-let locationInput = document.querySelector('#location')
 let locationBtn = document.querySelector('#location-btn')
-let locationLoader = document.querySelector('#location-loader')
 let canvasElement = document.querySelector('#canvas')
 let videoPlayer = document.querySelector('#player')
 let imagePickerArea = document.querySelector('#pick-image')
 let shareButton = document.querySelector('#share-button')
 let flipCameraButton = document.querySelector('#flip-camera-button')
-
-let picture
-let front = false
-
-const init = () => {
-  locationLoader.style.display = 'none'
-  canvasElement.style.display = 'none'
-  imagePickerArea.style.display = 'none'
-  const constraints = { video: { facingMode: (front ? 'user' : 'environment') }, audio: false }
-  initializeMedia(constraints)
-  initializeLocation()
-}
+let scanQRCode = document.querySelector('#qr-code-button')
+let output = document.querySelector('#output')
+let qrCodeBtn = document.querySelector('#qr-code-button')
+let retryBtn = document.querySelector('#retry-btn')
 
 const dataURItoBlob = (dataURI) => {
+  /* converts the picture to a blob in javascript */
   const byteString = atob(dataURI.split(',')[1]);
   const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
   const ab = new ArrayBuffer(byteString.length);
@@ -30,6 +21,19 @@ const dataURItoBlob = (dataURI) => {
   }
   const blob = new Blob([ab], {type: mimeString});
   return blob;
+}
+
+let picture // global for loading the picture into memory
+let front = false // bool value that specifies if the camera is facing front or back
+const defaultConstraints = { video: { facingMode: (front ? 'user' : 'environment') }, audio: false }
+// constraint object to specify default values for camera orientation
+
+const init = (constraints = defaultConstraints) => {
+  canvasElement.style.display = 'none'
+  imagePickerArea.style.display = 'none'
+  retryBtn.style.display = 'none'
+  initializeMedia(constraints)
+  initializeLocation()
 }
 
 const initializeLocation = () => {
@@ -67,22 +71,60 @@ const initializeMedia = (constraints) => {
     })
 }
 
-const snapshotHandler = event => {
-  canvasElement.style.display = 'block'
+const showButtons = () => {
+  snapshotButton.style.display = 'inline'
+  qrCodeBtn.style.display = 'inline'
+  retryBtn.style.display = 'none'
+}
+
+const hideButtons = () => {
   videoPlayer.style.display = 'none'
   snapshotButton.style.display = 'none'
+  qrCodeBtn.style.display = 'none'
+  canvasElement.style.display = 'block'
+  retryBtn.style.display = 'inline'
+}
+
+const freezeFrame = () => {
   let context = canvasElement.getContext('2d')
   context.drawImage(videoPlayer, 0, 0, canvas.width, videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width))
   videoPlayer.srcObject.getVideoTracks().forEach(track => {
     track.stop()
   })
+  return context
+}
+
+const clearPicture = () => {
+  /* this clears the canvasElement and cleans up for returning to normal camera mode after
+   * having taken a picture */
+  picture = null
+  const constraints = { video: { facingMode: (front ? 'user' : 'environment') } }
+  init(constraints)
+}
+
+const snapshotHandler = event => {
+  hideButtons()
+  _ = freezeFrame()
   picture = dataURItoBlob(canvasElement.toDataURL('image/webp'))
+}
+
+const scanQRCodeHandler = event => {
+  hideButtons()
+  const context = freezeFrame()
+  const currentImage = context.getImageData(0, 0, canvas.width, canvas.height)
+  qrCode = new jsQR(currentImage.data, currentImage.width, currentImage.height);
+  if (qrCode) {
+    output.value = qrCode.data || 'No QR detected, try moving closer'
+  } else {
+    output.value = 'You need to scan the image first'
+  }
 }
 
 const flipCameraHandler = event => {
   front = !front
   const constraints = { video: { facingMode: (front ? 'user' : 'environment') } }
-  initializeMedia(constraints)
+  init(constraints)
+  showButtons()
 }
 
 const getLocationHandler = event => {
@@ -92,20 +134,12 @@ const getLocationHandler = event => {
   let sawAlert = false
 
   locationBtn.style.display = 'none'
-  locationLoader.style.display = 'block'
 
   navigator.geolocation.getCurrentPosition(position => {
     locationBtn.style.display = 'inline'
-    locationLoader.style.display = 'none'
     fetchedLocation = {lat: position.coords.latitude, lng: position.coords.longitude}
-    locationInput.value = `At ${fetchedLocation.lat} latitude and ${fetchedLocation.lng} longitude`
+    output.value = `At Lat:${fetchedLocation.lat} and Long: ${fetchedLocation.lng}`
   }, err => {
-    locationBtn.style.display = 'inline'
-    locationLoader.style.display = 'none'
-    if (!sawAlert) {
-      console.error('Couldnt fetch location, please enter manually!')
-      sawAlert = true
-    }
     fetchedLocation = {lat: 0, lng: 0}
   }, {timeout: 7000})
 }
@@ -117,10 +151,17 @@ const shareHandler = event => {
   navigator.share({
     title: document.title,
     text: 'Hello from sharing',
-    url: 'https://developer.mozilla.org',
+    url: 'https://example.com',
   });
 }
 
+const retryHandler = event => {
+  showButtons()
+  clearPicture()
+}
+
+retryBtn.addEventListener('click', retryHandler)
+scanQRCode.addEventListener('click', scanQRCodeHandler)
 shareButton.addEventListener('click', shareHandler)
 snapshotButton.addEventListener('click', snapshotHandler)
 flipCameraButton.addEventListener('click', flipCameraHandler)
